a86fb36baeb62247182e7d05b13de392616eb03b
Dyn1k <kruglov1299@yandex.ru>
2022-06-30 15:42:47 +0300

    fixed MoviesService.js

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -4,71 +4,43 @@ class MoviesService {
     this.apiMovie = '/search/movie';
     this.apiGenres = '/genre/movie/list';
     this.apiGuestSession = '/authentication/guest_session/new';
-    this.apiKey = '2cc18ce7a3c09a8400d9c07212f86c7e';
+    this.apiKey = 'api_key=2cc18ce7a3c09a8400d9c07212f86c7e';
   }
 
-  async getResource(type, url = '') {
-    const res = await fetch(
-      `${this.apiBase}${type}?api_key=${this.apiKey}${url}`
-    );
+  getResource = async (url, option) => {
+    const res = await fetch(`${this.apiBase}${url}`, option);
     if (!res.ok) {
       throw new Error(`Could not fetch ${url}, received ${res.status}`);
     }
-    return res;
-  }
+    return res.json();
+  };
 
-  async getMovies(text, page = 1) {
-    const res = await this.getResource(
-      this.apiMovie,
-      `&query=${text}&page=${page}`
+  getMovies = async (text, page = 1) =>
+    this.getResource(
+      `${this.apiMovie}?${this.apiKey}&query=${text}&page=${page}`
     );
-    if (!res.ok)
-      throw new Error(`Could not get a list of movies, received ${res.status}`);
 
-    return res.json();
-  }
+  getGenres = async () => this.getResource(`${this.apiGenres}?${this.apiKey}`);
 
-  async getGenres() {
-    const res = await this.getResource(this.apiGenres);
-    if (!res.ok) {
-      throw new Error(`Could not get genres, received ${res.status}`);
-    }
-    return res.json();
-  }
+  createGuestSession = async () =>
+    this.getResource(`${this.apiGuestSession}?${this.apiKey}`);
 
-  async createGuestSession() {
-    const res = await this.getResource(this.apiGuestSession);
-    if (!res.ok)
-      throw new Error(`Could not create Guest Session, received ${res.status}`);
-
-    return res.json();
-  }
-
-  async rateMovie(movieId, guestId, rate) {
-    const body = {
-      value: rate,
-    };
-    await fetch(
-      `${this.apiBase}/movie/${movieId}/rating?api_key=${this.apiKey}&guest_session_id=${guestId}`,
+  rateMovie = async (movieId, guestId, rate) =>
+    this.getResource(
+      `/movie/${movieId}/rating?${this.apiKey}&guest_session_id=${guestId}`,
       {
         method: 'POST',
+        body: JSON.stringify({ value: rate }),
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
         },
-        body: JSON.stringify(body),
       }
-    ).catch((e) => `Could not get genres, received ${e.status}`);
-  }
-
-  async getRatedMovies(guestId, page = 1) {
-    const res = await fetch(
-      `${this.apiBase}/guest_session/${guestId}/rated/movies?api_key=${this.apiKey}&page=${page}`
     );
-    if (!res.ok) {
-      throw new Error(`Could not get rated movies, received ${res.status}`);
-    }
-    return res.json();
-  }
+
+  getRatedMovies = async (guestId, page = 1) =>
+    this.getResource(
+      `/guest_session/${guestId}/rated/movies?${this.apiKey}&page=${page}`
+    );
 }
 
 export default MoviesService;
